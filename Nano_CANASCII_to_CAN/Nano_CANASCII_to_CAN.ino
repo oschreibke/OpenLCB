ddd45ea6767b93e9a104ad5d536261a3ecb7c188
Otto Schreibke <otto@solwiz.ch>
2017-03-16 12:05:26 +0100

    Get Wifi to can working

--- Nano_CANASCII_to_CAN/Nano_CANASCII_to_CAN.ino
+++ Nano_CANASCII_to_CAN/Nano_CANASCII_to_CAN.ino
@@ -1,5 +1,5 @@
 //
-// Bridge CAN-ASCII (produced by JMRI-OpenLCB) over from a serial input to a physical CAN adapter
+// Bridge CAN-ASCII (produced by JMRI-OpenLCB) from a serial input (from ESP8266) to a physical CAN adapter
 //
 /*
    From: https://gridconnect.com/media/documentation/grid_connect/CAN_USB232_UM.pdf
@@ -41,6 +41,8 @@
 
 #include <SPI.h>
 
+//#define SHOWLCD 1
+
 // Arduino SPI Pins
 //
 // CLK  D13
@@ -57,13 +59,14 @@
 //#define GPIO14 14
 //#define GPIO15 15
 
+#if (SHOWLCD)
 #include <LiquidCrystal_I2C.h>
 /*
   Pins: SDA   A4
         SCK   A5
 */
 
-#define I2C_ADDR    0x3f 
+#define I2C_ADDR    0x3f
 #define BACKLIGHT_PIN     3
 #define En_pin  2
 #define Rw_pin  1
@@ -74,16 +77,17 @@
 #define D7_pin  7
 
 
-LiquidCrystal_I2C  lcd(I2C_ADDR,En_pin,Rw_pin,Rs_pin,D4_pin,D5_pin,D6_pin,D7_pin);
+LiquidCrystal_I2C  lcd(I2C_ADDR, En_pin, Rw_pin, Rs_pin, D4_pin, D5_pin, D6_pin, D7_pin);
+#endif
 
-
+bool initialised = false;
 enum WiFiDecodeStatus {expect_colon, expect_msg_type, process_identifier, process_data};
 enum CAN_message_type {Standard = 0, Extended = 1};
 WiFiDecodeStatus decodeStatus = expect_colon;
 CAN_message_type  WiFiMessageType = Standard;
 CAN_message_type  CANMessageType = Standard;
 
-MCP_CAN CAN(15); // Set CS to pin 15
+MCP_CAN CAN(10); // Set CS to pin 10
 
 volatile boolean can_msg_received = false;
 
@@ -138,167 +142,199 @@ char Int2Hex(int nybble) {
 void setup() {
   Serial.begin(115200);
 
-  lcd.begin (16,2); //  <<----- My LCD was 16x2
-  lcd.setBacklightPin(BACKLIGHT_PIN,POSITIVE);
+#if (SHOWLCD)
+  lcd.begin (16, 2); //  <<----- My LCD was 16x2
+  lcd.setBacklightPin(BACKLIGHT_PIN, POSITIVE);
   lcd.setBacklight(HIGH);
   lcd.clear();
-  
+#endif
   //start UART and the server
   //Serial.begin(115200);
 
-  if (CAN.begin(MCP_ANY, CAN_125KBPS, MCP_8MHZ) == CAN_OK)
+  initialised = (CAN.begin(MCP_ANY, CAN_125KBPS, MCP_8MHZ) == CAN_OK);
+  if (initialised) {
+#if (SHOWLCD)
     lcd.print("can init ok");
+#endif
+
+    CAN.setMode(MCP_NORMAL);
+
+    attachInterrupt(D2, MCP2515_ISR, FALLING); // start interrupt
+    
+//    Serial.println("can init ok");
+  }
+#if (SHOWLCD)
   else
     lcd.print("Can init fail");
-  CAN.setMode(MCP_NORMAL);
+#endif
 
-  attachInterrupt(D2, MCP2515_ISR, FALLING); // start interrupt
-
+//for (int x = 0; x < 16; x++){
+//  Serial.print(hexDigits[x]); Serial.print(": "); Serial.println(Hex2Int(hexDigits[x])); 
+//}
 }
 
 void loop() {
-    // Send incoming message from CAN to WiFi (if available)
-    if (can_msg_received) {
-      can_msg_received = false;
+  // Send incoming message from CAN to WiFi (if available)
+  if (can_msg_received) {
+    can_msg_received = false;
 
-      CAN.readMsgBuf(&CANIdentifier, &CANDataLen, CANData);      // Read data: len = data length, buf = data byte(s)
+    CAN.readMsgBuf(&CANIdentifier, &CANDataLen, CANData);      // Read data: len = data length, buf = data byte(s)
 
-      if ((CANIdentifier & 0x40000000) == 0x40000000) {  // Determine if message is a remote request frame.
-        sprintf(msgString, " REMOTE REQUEST FRAME");
-        lcd.clear();
-        lcd.print(msgString);
+    if ((CANIdentifier & 0x40000000) == 0x40000000) {  // Determine if message is a remote request frame.
+#if (SHOWLCD)
+      sprintf(msgString, " REMOTE REQUEST FRAME");
+      lcd.clear();
+      lcd.print(msgString);
+#endif
+    }
+    else {
+      if ((CANIdentifier & 0x80000000) == 0x80000000)  {  // Determine if ID is standard (11 bits) or extended (29 bits)
+        CANMessageType = Extended;
+        CANIdentifier = CANIdentifier & 0x1FFFFFFF;
       }
       else {
-        if ((CANIdentifier & 0x80000000) == 0x80000000)  {  // Determine if ID is standard (11 bits) or extended (29 bits)
-          CANMessageType = Extended;
-          CANIdentifier = CANIdentifier & 0x1FFFFFFF;
-        }
-        else {
-          CANMessageType = Standard;
-          CANIdentifier = CANIdentifier & 0x7FF;
-        }
-
-        for (int i = 0; i < 21; i ++) {
-          CANAscii[i] = '\0';  // clear the output string
-        }
-        int pos = 2 + ((CANMessageType == Standard) ? 3 : 8);
-        unsigned long id = CANIdentifier;
-       
-        CANAscii[0] = ':';
-        CANAscii[1] = ((CANMessageType == Standard) ? 'S' : 'X');
-        for (int i = pos -1; i >= 2; i--) {                 // write the identifier
-          CANAscii[2 + i] = hexDigits[id & 0x0F];
-          id = id << 4;
-        }
-        CANAscii[pos++] = 'N';                              // write the data (if any)
-        for (int i = 0; i < CANDataLen; i++) {
-          CANAscii[pos++] = hexDigits[(CANData[i] >> 4) & 0x0F];
-          CANAscii[pos++] = hexDigits[CANData[i] & 0x0F];
-        }
-        CANAscii[pos] = ';';                                // write the terminator
-        Serial.print((String)CANAscii);               // Transmit the CAN-Ascii message to the client
-        lcd.clear();
-        lcd.print("Sending to WiFi client: ");
-        lcd.println((String)CANAscii);
+        CANMessageType = Standard;
+        CANIdentifier = CANIdentifier & 0x7FF;
       }
+
+      for (int i = 0; i < 21; i ++) {
+        CANAscii[i] = '\0';  // clear the output string
+      }
+      int pos = 2 + ((CANMessageType == Standard) ? 3 : 8);
+      unsigned long id = CANIdentifier;
+
+      CANAscii[0] = ':';
+      CANAscii[1] = ((CANMessageType == Standard) ? 'S' : 'X');
+      for (int i = pos - 1; i >= 2; i--) {                // write the identifier
+        CANAscii[2 + i] = hexDigits[id & 0x0F];
+        id = id << 4;
+      }
+      CANAscii[pos++] = 'N';                              // write the data (if any)
+      for (int i = 0; i < CANDataLen; i++) {
+        CANAscii[pos++] = hexDigits[(CANData[i] >> 4) & 0x0F];
+        CANAscii[pos++] = hexDigits[CANData[i] & 0x0F];
+      }
+      CANAscii[pos] = ';';                                // write the terminator
+      Serial.print((String)CANAscii);               // Transmit the CAN-Ascii message to the client
+#if (SHOWLCD)
+      lcd.clear();
+      lcd.print("Sending to WiFi client: ");
+      lcd.println((String)CANAscii);
+#endif
     }
+  }
 
-    // send incoming message from WiFi to CAN  (if available)
-    if (Serial.available()) {
-      //get data from the telnet client
-      while (Serial.available()) {
-        char ch = Serial.read();
-        lcd.print(ch); // push it to the UART (=> Serial Monitor)
-        switch (decodeStatus) {
-          case expect_colon:
-            // if we receive a : change the status, initialise the data stores and discard the character
-            // otherwise ignore the character - it's not what we were expecting
-            if (ch = ':') {
-              decodeStatus = expect_msg_type;
+  // send incoming message from WiFi to CAN  (if available)
+  if (Serial.available()) {
+    //get data from the telnet client
+    while (Serial.available()) {
+      char ch = Serial.read();
+#if (SHOWLCD)
+      lcd.print(ch); // push it to the UART (=> Serial Monitor)
+#endif
+      //Serial.write(ch);
+      switch (decodeStatus) {
+        case expect_colon:
+          // if we receive a : change the status, initialise the data stores and discard the character
+          // otherwise ignore the character - it's not what we were expecting
+          if (ch == ':') {
+            decodeStatus = expect_msg_type;
+            WiFiMessageType = Standard;
+            WiFiIdentifier = 0;
+            WiFiDataLen = 0;
+            firstNybble = true;
+            //Serial.println("Got colon");
+          }
+          break;
+
+        case (expect_msg_type):
+          // the message Type indicators are S (Standard) and X (Extended)
+          // if we receive one of these, set the message type accordingly and change the status
+          // otherwise something is wrong therefore we reset to expecting_colon
+          decodeStatus = process_identifier;  // assume a valid message type
+          switch (ch) {
+            case 'S':
               WiFiMessageType = Standard;
-              WiFiIdentifier = 0;
-              WiFiDataLen = 0;
-              firstNybble = true;
-            }
-            break;
-
-          case (expect_msg_type):
-            // the message Type indicators are S (Standard) and X (Extended)
-            // if we receive one of these, set the message type accordingly and change the status
-            // otherwise something is wrong therefore we reset to expecting_colon
-            switch (ch) {
-                decodeStatus = process_identifier;  // assume a valid message type
-              case 'S':
-                WiFiMessageType = Standard;
-                break;
-
-              case 'X':
-                WiFiMessageType = Extended;
-                break;
-
-              default:
-                // we shouldn't be here - reset the processor
-                decodeStatus = expect_colon;
-                break;
-            }
-            break;
-
-          case (process_identifier):
-            // read hex chars until we hit an N
-            if (ch == 'N') {
-              decodeStatus = process_data;
               break;
-            }
 
-            // convert the identifier from Hex to Int
-            i = Hex2Int(ch);
-            if (i > 15) {
-              // not a hex character => reset the processor
+            case 'X':
+              WiFiMessageType = Extended;
+              break;
+
+            default:
+              // we shouldn't be here - reset the processor
               decodeStatus = expect_colon;
               break;
-            }
-            WiFiIdentifier = (WiFiIdentifier << 4) + i;
+          }
+          //Serial.println("Got Message type");
+          break;
+
+        case (process_identifier):
+          // read hex chars until we hit an N
+          if (ch == 'N') {
+            decodeStatus = process_data;
+            //Serial.print("Got Identifier "); Serial.println(WiFiIdentifier);
             break;
+          }
 
-          case (process_data):
-            // process data bytes (Hex encoded) until we hit a ;
-            if (ch == ';') {
-              // received a ; - send the message and reset the processor for the next
-              byte sndStat = CAN.sendMsgBuf(WiFiIdentifier, (int) WiFiMessageType, WiFiDataLen, WiFiData);
-              if (sndStat == CAN_OK) {
-                lcd.println("Message Sent Successfully!");
-              } else {
-                lcd.println("Error Sending Message...");
-              }
-
-              decodeStatus = expect_colon;
-              break;
-            }
-
-            if (firstNybble) {
-              WiFiData[WiFiDataLen] = 0x0;
-              WiFiDataLen++;
-            }
-
-            firstNybble = !firstNybble;
-            i = Hex2Int(ch);
-            if (i > 15) {
-              // not a hex character => reset the processor
-              decodeStatus = expect_colon;
-              break;
-            }
-
-            // WiFiDataLen contains the actual length, which is 1 greater than the index value
-            WiFiData[WiFiDataLen - 1] = (WiFiData[WiFiDataLen - 1] << 4) + i;
-            break;
-
-          default:
-            // should not be here - just reset the processor
+          // convert the identifier from Hex to Int
+          i = Hex2Int(ch);
+          //Serial.print(ch); Serial.print(": ");Serial.println(i);
+          if (i > 15) {
+            // not a hex character => reset the processor
             decodeStatus = expect_colon;
             break;
-        }
+          }
+          WiFiIdentifier = (WiFiIdentifier << 4) + i;
+          //Serial.println(WiFiIdentifier);
+          break;
+
+        case (process_data):
+          // process data bytes (Hex encoded) until we hit a ;
+          if (ch == ';') {
+            // received a ; - send the message and reset the processor for the next
+            byte sndStat = CAN.sendMsgBuf(WiFiIdentifier, (int) WiFiMessageType, WiFiDataLen, WiFiData);
+#if (SHOWLCD)
+            if (sndStat == CAN_OK) {
+              lcd.println("Message Sent Successfully!");
+            } else {
+              lcd.println("Error Sending Message...");
+            }
+#endif
+            //if (sndStat == CAN_OK) {
+            //  Serial.println("Message Sent Successfully!");
+            //} else {
+            //  Serial.println("Error Sending Message...");
+            //}
+            
+            decodeStatus = expect_colon;
+            break;
+          }
+
+          if (firstNybble) {
+            WiFiData[WiFiDataLen] = 0x0;
+            WiFiDataLen++;
+          }
+
+          firstNybble = !firstNybble;
+          i = Hex2Int(ch);
+          if (i > 15) {
+            // not a hex character => reset the processor
+            decodeStatus = expect_colon;
+            break;
+          }
+
+          // WiFiDataLen contains the actual length, which is 1 greater than the index value
+          WiFiData[WiFiDataLen - 1] = (WiFiData[WiFiDataLen - 1] << 4) + i;
+          break;
+
+        default:
+          // should not be here - just reset the processor
+          decodeStatus = expect_colon;
+          break;
       }
     }
-  
+  }
+
 }
 
